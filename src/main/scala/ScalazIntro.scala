f1c5ebe5b02371dedb5878d2eddb0c374f95c689
Teijo Laine <teijo@aropupu.fi>
2015-08-06 15:08:20 +0300

    Applicative functor

--- src/main/scala/ScalazIntro.scala
+++ src/main/scala/ScalazIntro.scala
@@ -3,6 +3,15 @@ import scalaz.std.anyVal._
 import scalaz.std.list._
 
 object ScalazIntro {
+  def testApplicative() {
+    val a = Applicative[List]
+    assert(List(1) == a.point(1))
+    assert(List(2, 3, 4) == a.map(List(1, 2, 3))(_ + 1))
+
+    val result = a.ap(a.point(1))(List({ i: Int => i + 1 }, { i: Int => i - 1 }))
+    assert(List(2, 0) == result)
+  }
+
   def testFunctor() {
     val result = List[Int](1, 2, 3).map { x: Int => x + 1 }
     assert(List(2, 3, 4) == result)
@@ -18,6 +27,7 @@ object ScalazIntro {
   }
 
   def main(args: Array[String]) {
+    testApplicative()
     testFunctor()
     testMonoid()
   }
